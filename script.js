5436a11965420cf46d1d4327fbb2da3d2829a2b4
krish <karora07@student.ubc.ca>
2023-05-16 23:59:28 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// var toggleButton = document.getElementById("about");
+// var hiddenText = document.getElementById("hiddenText");
+
+// toggleButton.addEventListener("click", function() {
+//   if (hiddenText.style.display === "none") {
+//     hiddenText.style.display = "block";
+//   } else {
+//     hiddenText.style.display = "none";
+//   }
+// });
